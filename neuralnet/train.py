05cd0126e42f3e075e144545810594972889c67a
Weston Willingham <westonwillingham@gmail.com>
2021-04-22 18:35:31 -0400

    commit

--- neuralnet/train.py
+++ neuralnet/train.py
@@ -26,11 +26,12 @@ class SpeechModule(LightningModule):
         return self.model(x, hidden)
 
     def configure_optimizers(self):
+        self.metric = "accuracy"
         self.optimizer = optim.AdamW(self.model.parameters(), self.args.learning_rate)
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-                                        self.optimizer, mode='min',
-                                        factor=0.50, patience=6)
-        return [self.optimizer], [self.scheduler]
+                                       self.optimizer, mode='min',
+                                       factor=0.50, patience=6)
+        return [self.optimizer], [self.scheduler], [self.metric]
 
     def step(self, batch):
         spectrograms, labels, input_lengths, label_lengths = batch 
